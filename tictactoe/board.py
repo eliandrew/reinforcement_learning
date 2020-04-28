3e230441cc5bce70f8f58419d099ac8157444ce9
samuel-cowin <scowin23@gmail.com>
2020-04-28 08:32:20 -0700

    Temporary changes to the board file

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -1,22 +1,76 @@
+import copy
 
 class Board:
 
-    @staticmethod
-    def display(state):
+    def __init__(self):
+        self.dim = 3
+        self.board_space = [[0]*self.dim]*self.dim
+        self.current_player = 1
+
+    def update_board(self, coord)
+        """From the current state and action, return the remaining state of the board
+        Parameters
+        ----------
+        coord: tuple(int,int)
+          coordinate representing a potential move of the players
+
+        Return
+        ------
+        board: list[list[int]]
+          Returns a copy of the board representing the possible state given the action
+        """
+        board = self.board_space
+
+    def end_of_game(self)
+        """Determines if the game is over whether by tie or there is a winner
+
+        Return
+        ------
+        game_over: bool
+          boolean to determine if the game is over 
+        """
+        def generate_slices(self):
+          """Takes the current board space and creates all slices of rows, columns, and diagonals.
+
+          Return
+          ------
+          board_slices: list[list[int]]
+            Slices of the board representing rows, columns, and diagonals 
+          """
+          board_slices = self.board_space
+          for i in self.dim:
+            for j in self.dim:
+              col.append(self.board_space[j][i])
+              if i == j:
+                diag.append(self.board_space[i][j])
+              elif 
+            board_slices.append(col)
+
+        def all_same_player(board_slice)
+        """Determines if the slice from the board is all from the same player 
+
+        Parameters
+        ----------
+        board_slice: list[int]
+          Slice of the board that is either a row, column, or diagonal represented by integer values of
+          0, 1, or 2 depending on the player moves.
+          If they are all the same, then there is a winner.
+        
+        Return
+        ------
+        : bool
+          Indicates if there is a winner by returning True or False
+        """
+          return board_slice == [1, 1, 1] or board_slice == [2, 2, 2]
+
+    def display(self):
         """Displays the current state of the board using ASCII art. 
 
         X | X | X
         - | - | -
         O | O | O
         - | - | -
-        X | X | x
-
-
-        Parameters
-        ----------
-        state: list[list[int]] 
-          A 2D list with dimensions equal to the dimensions of the board.
-          Values should be 0 for empty, 1 for player X, or 2 for player O.
+        X | X | X
 
         Returns
         -------
@@ -43,5 +97,5 @@ class Board:
             """
             return "|".join([value_markers[i] for i in row]) + "\n"
 
-        board_display = row_separator.join([display_row(r) for r in state])
+        board_display = row_separator.join([display_row(r) for r in self.board_space])
         return board_display
