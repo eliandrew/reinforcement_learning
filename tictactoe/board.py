da1c7902fc6090429372b4e84dc9c3ed021a2365
Eli Andrew <e.andrew@salesforce.com>
2020-04-29 08:09:17 -0700

    Finish board logic

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -1,13 +1,42 @@
-import copy
+import numpy as np
+import random
+
 
 class Board:
+    """Represents the tic-tac-toe board. This includes: board display,
+    board state calculation, and board updates. 
+
+    Attributes
+    ----------
+    dim: int
+      The dimension of the board. In tic-tac-toe this is a 3x3 board.
+
+    board_space: np.array[np.array[int]]
+      The current state of the board.
+
+    current_player: int
+      The current player who can make a turn. 1 is for X and 2 is for O.
+    """
 
     def __init__(self):
         self.dim = 3
-        self.board_space = [[0]*self.dim]*self.dim
+        self.board_space = np.zeros((self.dim, self.dim), dtype=int)
         self.current_player = 1
 
-    def update_board(self, coord)
+    def play_turn(self, state):
+        """Determines the new board state after playing the given coord for the 
+        current player. This will then set its board_space to the new board and
+        update the current player.
+
+        Parameters
+        ----------
+        coord: tuple(int, int)
+          The coordinate on the board that should be played
+        """
+        self.board_space = state
+        self.current_player = 1 if self.current_player is 2 else 2
+
+    def calculate_board(self, coord):
         """From the current state and action, return the remaining state of the board
         Parameters
         ----------
@@ -16,52 +45,77 @@ class Board:
 
         Return
         ------
-        board: list[list[int]]
+        board: np.array[np.array[int]]
           Returns a copy of the board representing the possible state given the action
         """
-        board = self.board_space
+        board = np.copy(self.board_space)
+        x, y = coord
+        board[x, y] = self.current_player
+        return board
 
-    def end_of_game(self)
+    def generate_slices(self):
+        """Takes the current board space and creates all slices of rows, columns, and diagonals.
+
+        Return
+        ------
+        board_slices: np.array[np.array[int]]
+          Slices of the board representing rows, columns, and diagonals
+        """
+        row_slices = np.copy(self.board_space)
+        col_slices = np.transpose(np.copy(self.board_space))
+        diag_slices_one = np.diagonal(np.copy(self.board_space))
+        diag_slices_two = np.diagonal(
+            np.flip(np.copy(self.board_space), axis=0))
+
+        board_slices = np.vstack(
+            (row_slices, col_slices, [diag_slices_one], [diag_slices_two]))
+        return board_slices
+
+    def end_of_game(self):
         """Determines if the game is over whether by tie or there is a winner
 
         Return
         ------
         game_over: bool
-          boolean to determine if the game is over 
+          boolean to determine if the game is over
         """
-        def generate_slices(self):
-          """Takes the current board space and creates all slices of rows, columns, and diagonals.
 
-          Return
-          ------
-          board_slices: list[list[int]]
-            Slices of the board representing rows, columns, and diagonals 
-          """
-          board_slices = self.board_space
-          for i in self.dim:
-            for j in self.dim:
-              col.append(self.board_space[j][i])
-              if i == j:
-                diag.append(self.board_space[i][j])
-              elif 
-            board_slices.append(col)
+        def all_same_player(board_slice):
+            """Determines if the slice from the board is all from the same player
 
-        def all_same_player(board_slice)
-        """Determines if the slice from the board is all from the same player 
+                Parameters
+                ----------
+                board_slice: np.array[int]
+                  Slice of the board that is either a row, column, or diagonal represented by integer values of
+                  0, 1, or 2 depending on the player moves.
+                  If they are all the same, then there is a winner.
 
-        Parameters
-        ----------
-        board_slice: list[int]
-          Slice of the board that is either a row, column, or diagonal represented by integer values of
-          0, 1, or 2 depending on the player moves.
-          If they are all the same, then there is a winner.
-        
-        Return
-        ------
-        : bool
-          Indicates if there is a winner by returning True or False
+                Return
+                ------
+                : bool
+                  Indicates if there is a winner by returning True or False
+                """
+            return np.array_equal(board_slice, [1, 1, 1]) or np.array_equal(board_slice, [2, 2, 2])
+
+        board_slices = self.generate_slices()
+        game_over = np.any([all_same_player(s) for s in board_slices])
+        return game_over
+
+    def potential_next_states(self):
+        """Determines what states can be entered next given the current state of the board
+        and the current player. The states returned are represented as a board_state which
+        is just a 2D np.array.
+
+        Returns
+        -------
+        next_states: 3D np.array
+          The potential next states we can enter from the current state. Outer dimension
+          is all possible states, and the inner two dimensions are the states themselves.
         """
-          return board_slice == [1, 1, 1] or board_slice == [2, 2, 2]
+        x_coords, y_coords = np.where(self.board_space == 0)
+        next_states = [self.calculate_board(coord)
+                       for coord in zip(x_coords, y_coords)]
+        return next_states
 
     def display(self):
         """Displays the current state of the board using ASCII art. 
@@ -87,7 +141,7 @@ class Board:
 
             Parameters
             ----------
-            row: list[int]
+            row: np.array[int]
               A single row of the overall board.
 
             Returns
@@ -97,5 +151,18 @@ class Board:
             """
             return "|".join([value_markers[i] for i in row]) + "\n"
 
-        board_display = row_separator.join([display_row(r) for r in self.board_space])
+        board_display = row_separator.join(
+            [display_row(r) for r in self.board_space])
         return board_display
+
+    def fake_game(self):
+        while not self.end_of_game():
+            print(self.display())
+            if self.current_player == 1:
+                next_states = self.potential_next_states()
+                state = random.choice(next_states)
+                self.play_turn(state)
+            else:
+                x = input("x: ")
+                y = input("y: ")
+                self.play_turn(self.calculate_board((int(x), int(y))))
