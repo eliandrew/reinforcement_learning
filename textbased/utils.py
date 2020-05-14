4a6af9e367a38c3f3b422a71c932b0d03f484823
Eli Andrew <e.andrew@salesforce.com>
2020-05-14 06:41:28 -0700

    Finished

--- textbased/utils.py
+++ textbased/utils.py
@@ -2,6 +2,18 @@ import numpy as np
 from collections import defaultdict
 
 
+def epsilon_greedy(q_k, env, epsilon):
+    """
+    From Q values generated, return a stochastic epsilon greedy policy
+    """
+    def pi(s):
+        a = env.action_space.sample()
+        best = max(q_k[s], key=q_k[s].get) if len(q_k[s]) > 0 else a
+        return np.random.choice([a, best], p=[epsilon, 1-epsilon])
+
+    return pi
+
+
 def display_values(v, n, m):
     """Displays the values in v on an nxm grid.
     """
@@ -24,3 +36,31 @@ def state_values_to_action_values(v_pi, env, gamma=1.0):
                               for prob, s_prime, r, _ in env.P[s][a]])
 
     return q_pi
+
+
+def combine_nested_dicts(a, b):
+    """Combines the 2D dicts of floats a and b by adding together values for
+    each key.
+    """
+    result = a.copy()
+    for outer_key in b:
+        for inner_key in b[outer_key]:
+            result[outer_key][inner_key] += b[outer_key][inner_key]
+
+    # result = defaultdict(lambda: defaultdict(float))
+    # for outer_key, inner_dict in a.items():
+    #     for inner_key, value in inner_dict.items():
+    #         result[outer_key][inner_key] += value + b[outer_key][inner_key]
+    # for outer_key, inner_dict in b.items():
+    #     if outer_key not in result:
+    #         for inner_key, value in inner_dict.items():
+    #             if inner_key not in result[outer_key]:
+    #                 result[outer_key][inner_key] += value
+    return result
+
+
+def initial_pi(env):
+    """
+    This returns an arbitrary pi for a given environment
+    """
+    return lambda s: env.action_space.sample()
--- textbased/utils_Q.py
+++ textbased/utils_Q.py
@@ -1,21 +1,10 @@
 import numpy as np
-from collections import defaultdict
+from collections import Counter, defaultdict
+import utils
 import time
 
 
-def epsilon_greedy(q_k, env, epsilon):
-    """
-    From Q values generated, return a stochastic epsilon greedy policy
-    """
-    def pi(s):
-        a = env.action_space.sample()
-        best = max(q_k[s], key=q_k[s].get) if len(q_k[s]) > 0 else a
-        return np.random.choice([a, best], p=[epsilon, 1-epsilon])
-
-    return pi
-
-
-def Q_learning(env, nE, epsilon_0=1.0, alpha=0.01, gamma=0.9, debug=False, render=False):
+def q_learning(env, nE, min_epsilon=0.01, alpha=0.01, gamma=0.9, debug=False, render=False):
     """
     This method updates the state action values using the Q-learning algorithm
 
@@ -23,27 +12,76 @@ def Q_learning(env, nE, epsilon_0=1.0, alpha=0.01, gamma=0.9, debug=False, rende
     """
 
     q_pi = defaultdict(lambda: defaultdict(float))
-    min_epsilon = 0.05
 
     for e in range(nE):
+
+        s = env.reset()
+        epsilon = (1.0-min_epsilon) * \
+            (1.0-float(e)/float(nE)) + min_epsilon
+        done = False
+
         if debug:
             if e % 1000 == 0:
                 print("Completed {} episodes".format(e))
-
-        s = env.reset()
-        epsilon = (epsilon_0-min_epsilon) * \
-            (1.0-float(e+1)/float(nE)) + min_epsilon
-        done = False
+                print("Epsilon {}".format(epsilon))
 
         while not done:
             if render:
                 env.render()
                 time.sleep(0.25)
-            a = epsilon_greedy(q_pi, env, epsilon)(s)
+            a = utils.epsilon_greedy(q_pi, env, epsilon)(s)
             s_prime, r, done, _ = env.step(a)
-            a_target = epsilon_greedy(q_pi, env, 0)(s_prime)
+            a_target = utils.epsilon_greedy(q_pi, env, 0)(s_prime)
 
             q_pi[s][a] += alpha*(r + gamma*q_pi[s_prime][a_target]-q_pi[s][a])
             s = s_prime
 
-    return q_pi, epsilon_greedy(q_pi, env, min_epsilon)
+    return q_pi, utils.epsilon_greedy(q_pi, env, min_epsilon)
+
+
+def double_q_learning(env, nE, alpha=0.01, gamma=0.9, min_epsilon=0.01, debug=False):
+    """Learns Q use the double-q learning algorithm:
+
+     choose A using Q_1 + Q_2
+
+     with p=.5 make update to Q_1 using Q_2:
+
+         Q_1(S, A) = Q_1(S, A) + alpha(r + gamma * Q_2(S', pi_t(S')) - Q_1(S, A))
+
+     with p=.5 make update to Q_2 using Q_1:
+
+         Q_2(S, A) = Q_2(S, A) + alpha(r + gamma * Q_1(S', pi_t(S')) - Q_2(S, A))
+     """
+
+    q_1 = defaultdict(lambda: defaultdict(float))
+    q_2 = defaultdict(lambda: defaultdict(float))
+    for e in range(nE):
+
+        epsilon = (1.0 - min_epsilon) * \
+            (1.0 - float(e)/float(nE)) + min_epsilon
+
+        if debug:
+            if e % 1000 == 0:
+                print("Completed {} episodes".format(e))
+                print("Epsilon: {}".format(epsilon))
+        done = False
+        s = env.reset()
+
+        while not done:
+            a = utils.epsilon_greedy({s:
+                                      Counter(q_1[s]) + Counter(q_2[s])}, env, epsilon)(s)
+            s_prime, r, done, _ = env.step(a)
+
+            if np.random.rand() < 0.5:
+                a_prime = utils.epsilon_greedy(q_2, env, epsilon=0)(s_prime)
+                q_1[s][a] += alpha * \
+                    (r + gamma * q_2[s_prime][a_prime] - q_1[s][a])
+            else:
+                a_prime = utils.epsilon_greedy(q_1, env, epsilon=0)(s_prime)
+                q_2[s][a] += alpha * \
+                    (r + gamma * q_1[s_prime][a_prime] - q_2[s][a])
+
+            s = s_prime
+
+    q_pi = utils.combine_nested_dicts(q_1, q_2)
+    return q_pi, utils.epsilon_greedy(q_pi, env, epsilon=min_epsilon)
